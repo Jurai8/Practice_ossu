771cc712b9aa70e585763892eceef569f81f715b
Charles <131194505+Jurai8@users.noreply.github.com>
2024-11-30 07:10:21 +0200

    n

--- Ossu/bisection-search.py
+++ Ossu/bisection-search.py
@@ -3,35 +3,41 @@
 def main():
 
     # the number the computer must find
-    my_number = 67
+    my_number = 91
 
     print("Please think of a number between 1 - 100\n")
     # define the range (high and low)
     low = 0
     high = 100
 
+    answer = ''
+
     # define the first guess
     # make sure it's an int
     guess = round((high + low)/2)
 
-    # while guess != my_number:
-        # print("Is your secret number", guess + "?")
+    while answer != 'c':
+        print("Is your secret number", guess, "?")
 
-        # answer = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+        answer = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. \n")
 
-        # if answer == 'h': ## if the guess is too high
-            ## decrease the high to the value of guess
-            # high = guess
-            # guess = round((high + low)/ 2)
+        # if the guess is too high
+        if answer == 'h': 
+            # decrease the high to the value of guess
+            high = guess
+            guess = round((high + low)/ 2)
+        # if the guess was too low
+        elif answer == 'l': 
+            # increase the low to the value of guess
+            low = guess
+            guess = round((high + low) / 2)
+        # if the answer is correct
+        elif answer  == 'c': 
+            print(f"Game over. Your secret number was: {guess}")
+        else:
+            print("I can't understand your input")
 
-        # elif answer == 'l' ## if the guess was too low
-            ## increase the low to the value of guess
-            # low = guess
-            # guess = round((high + low) / 2)
-
-        # elif answer  == 'c' ## if the answer is correct
-            # print("Game over. Your secret number was: " + guess)
-            # return
+    return
 
 
 
